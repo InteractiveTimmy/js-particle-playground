616d8f9684adaeb1cd992cc66eed89266b9a6ca6
InteractiveTimmy <InteractiveTimmy@users.noreply.github.com>
2019-02-08 21:33:35 -0500

    installed dependencies and configured rollup

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+export default {
+  input: 'src/jpp.js',
+  output: [
+    {
+      format: 'umd',
+      name: 'JPP',
+      file: 'build/jpp.js',
+      indent: '\t'
+    },
+    {
+      format: 'umd',
+      name: 'SPT',
+      file: 'examples/jpp.js',
+      indent: '\t'
+    },
+    {
+      format: 'es',
+      file: 'build/jpp.module.js',
+      indent: '\t'
+    }
+  ],
+  watch: {
+    include: 'src/**'
+  }
+};
